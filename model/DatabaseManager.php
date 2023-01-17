9061ace28942b822234b2a18ef078f50d0464056
Jonathan Mendoza <jonathan.mendoza.14@sansano.usm.cl>
2023-01-16 21:13:01 -0300

    Changed Connection class for DatabaseManager class
    -DatabaseManager will be the class that interacts with the database including connections, searchers, creation of new registers and update. Parameters will be provided by ActiveRecord.
    
    -Implementation of all() method.

--- model/DatabaseManager.php
+++ model/DatabaseManager.php
@@ -5,18 +5,20 @@ namespace Jonathan\PhpActiveRecord;
 use PDO;
 use PDOException;
 
-//Clase que maneja la conexion de la BD utilizando PDO Mysql.
-//Return $conn (instancia de una conexion).
-class Connection extends Singleton{
+//Clase que maneja la conexion y operaciones en la BD utilizando PDO Mysql.
+class DatabaseManager extends Singleton{
 
     //Variable que mantendra la conexion.
     private static $conn;
+
+    //Variable que contiene la whitelist de nombres de tablas para poder consultar.
+    private static $whiteListTables = [];
 
     //Variable que contiene configuraciones de la conexion con PDO.
     private static $PDO_OPTIONS = array(
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 
-    //Funcion para obtener la conexion a la BD.
+    //Funcion para setear la conexion a la BD.
     //Crea una nueva conexion si es que no se ha creado una instancia previamente.
     //Retorna true si existe o se creo una conexion exitosa. Retorna false si no se pudo realizar la conexion.
     //Se puede cambiar posteriormente para leer parametros con ENV.
@@ -30,21 +32,46 @@ class Connection extends Singleton{
                     self::$conn->setAttribute($key, $value);
                 }
 
+                try {
+                    $result = self::$conn->query('SHOW TABLES');
+                    self::$whiteListTables = $result->fetchAll(PDO::FETCH_COLUMN);
+                } catch (\Throwable $e) {
+                    echo 'Error recuperando los nombres de las tablas de la base de datos: ' . $e->getMessage();
+                }
+
             } catch (PDOException $e) {
                 echo 'Error en la conexion: ' . $e->getMessage();
                 return false;
             }
         }
-
         return true;
     }
 
+    //Funcion para obtener la conexion a la BD.
     public static function getConn() : PDO{
         if (!isset(self::$conn)) {
-            echo 'No existe una conexion seteada.\n';
+            echo 'No existe una conexion seteada.';
         }
 
         return self::$conn;
+    }
+
+    //Funcion para buscar todos los registros de una tabla especifica de la bd.
+    //Retorna un array asociativo con todos los registros.
+    //Los cuales a su vez son un array asociativo con cada registro de la tabla.
+    public static function all($tableName) : array{
+        $res = [];
+
+        if (in_array($tableName, self::$whiteListTables)){
+            $query = "SELECT * FROM {$tableName}";
+            $stmt = self::$conn->prepare($query);
+            $stmt->execute();
+
+            $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $stmt = null;
+        }
+
+        return $res;
     }
 
 }
